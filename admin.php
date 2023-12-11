ccc44569fede163ee5a9083ef46bc2359334dab0
Daniela2-1998 <danimansilla27@gmail.com>
2023-12-10 23:23:38 -0300

    filtro de busqueda por nombre lista

--- admin.php
+++ admin.php
@@ -51,8 +51,6 @@ else {
         <div class="container">
             <div class="row espaciado">
                 <?php
-
-                    // 1) Conexion
                     $conexion = mysqli_connect("127.0.0.1", "root", "");
                     mysqli_select_db($conexion, "tienda");
 
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/botonVolver.css">
 
      <!-- CSS del bootstrap  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -17,10 +18,12 @@
     </section>
 
     <section class="opciones-admin">
-        <div class="espacio-busqueda">
+        <form class="espacio-busqueda" method="post">
             <input class="busqueda" name="busqueda" type="text" placeholder="Buscar por título...">
+            <?php $busqueda = isset($_POST['busqueda']) ? $_POST['busqueda'] : null ; ?>
+
             <button class="botones boton-busqueda">Buscar</button>
-        </div>
+        </form>
 
 
         <a href="login.html">
@@ -40,12 +43,14 @@
         <div class="container">
             <div class="row espaciado">
                 <?php
-                    // 1) Conexion
                     $conexion = mysqli_connect("127.0.0.1", "root", "");
                     mysqli_select_db($conexion, "tienda");
 
+                    if(!$busqueda){
+
                     $consulta = 'SELECT * FROM videojuegos';
                     $datos = mysqli_query($conexion, $consulta);
+
 
                     while ($reg = mysqli_fetch_array($datos)) {?>
                         <div class="diseño-card card col-sm-12 col-md-5 col-lg-3">
@@ -61,7 +66,7 @@
                         </div>
 
 
-                        <a href="ver.php?id=<?php echo $reg['id']; ?>" class="card-body">
+                        <a class="card-body">
                             <h3 class="nombre" style="width: 100%; font-size:25px;">
                             <?php echo ucwords($reg['nombre']) ?>
                             </h3>
@@ -71,7 +76,44 @@
                        </a>
                     </div>
 
-                <?php } ?>
+                <?php } } 
+                 else{
+
+                  $nombre = $busqueda;
+
+                  $consulta2 = "SELECT * FROM videojuegos WHERE nombre='$nombre'";
+                  $datos2 = mysqli_query($conexion, $consulta2);
+
+                  while ($reg = mysqli_fetch_array($datos2)) {?>
+                    <div class="diseño-card card col-sm-12 col-md-5 col-lg-3">
+                    <div class= "">
+                      <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img class="card-img-top" src=" <?php echo ($reg['imagen'])?>" alt="" width="400px" height="400px") class="d-block w-100" alt="...">
+                        </div>
+          
+                      </div>
+                    </div>
+                    </div>
+
+
+                    <a class="card-body">
+                        <h3 class="nombre" style="width: 100%; font-size:25px;">
+                        <?php echo ucwords($reg['nombre']) ?>
+                        </h3>
+                        <p><?php echo $reg['consola']; ?></p>
+                        <p>$<?php echo $reg['precio']; ?></p>
+                        <p>En stock:<?php echo $reg['stock']; ?></p>
+                   </a>
+                </div>
+
+                <a href="index.php">
+                  <button class="boton-inicio-sesion boton-volver">Volver</button>
+                </a>
+                <?php } 
+
+              }?>
     
             </div>
         </div>
